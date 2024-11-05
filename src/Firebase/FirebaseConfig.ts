ea416fc3d931e0897ccaa44ad3fa6be7350a392d
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-11-05 20:56:31 +0530

    Fixed issue on Analytics

--- src/Firebase/FirebaseConfig.ts
+++ src/Firebase/FirebaseConfig.ts
@@ -1,19 +1,31 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-import {getAnalytics} from 'firebase/analytics'
-
+import { getAnalytics, isSupported } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-
 export const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
 export const auth = getAuth(firebaseApp);
+
+let analytics;
+
+const initializeAnalytics = async () => {
+  if (typeof window !== "undefined") {
+    const isAnalyticsSupported = await isSupported(); 
+    if (isAnalyticsSupported) {
+      analytics = getAnalytics(firebaseApp);
+    }
+  }
+};
+
+initializeAnalytics(); 
+
+export { analytics }; 
